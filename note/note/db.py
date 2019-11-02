fd2a3f9aa46bb69c5b5c35de87519d302aae48a1
TaniaZwe <tat.pogrebnyak@gmail.com>
2019-11-02 12:16:49 +0300

    api and class Note added

---
+++ note/note/db.py
@@ -0,0 +1,10 @@
+from note.db import db
+
+class Note(db.Model):
+    note_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=True)
+    creation_dt = db.Column(db.DateTime, nullable=False)
+    tags = db.Column(db.String, nullable=True)
